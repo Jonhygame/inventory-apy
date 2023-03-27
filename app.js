9670055a850c0ca70165a756192cb78ac43558b2
Jonathan Josafat Contreras Villanueva <contreras_villanueva@live.com.mx>
2023-03-27 11:07:29 -0600

    Primer commit solo tengo lo basico el servidor y un routes

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+//const MongoClient = require('mongodb').MongoClient
+//const conn_String = 'mongodb+srv://JonathanJ:420019jJ@cluster0.oqnajti.mongodb.net/?retryWrites=true&w=majority'
+app.use(express.json());
+//const Rutas = require('./routes/web');
+//app.use('/api/v1',Rutas);
+//const { Connection } = require('./Connection');
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+  try {
+    mongoose.connect(process.env.CONN_STRING).then(()=>{
+      const Cat = mongoose.model('Cat', { name: String });
+      const kitty = new Cat({ name: 'Zildjian' });
+      kitty.save().then(() => console.log('meow'));
+    });
+    console.log('Connected to Mongo');
+  } catch (error) {
+    console.log('Error connecting to Mongo'+ error)
+  }
+})
