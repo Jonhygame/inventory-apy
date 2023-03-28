6e6d8035b5b90f5989b494f3eda241c40bd9f645
Jonathan Josafat Contreras Villanueva <contreras_villanueva@live.com.mx>
2023-03-28 10:09:43 -0600

    Cambie algunos detalles de la app y separe el modelo y algunas rutas

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const mongoose = require('mongoose');
-require('dotenv').config();
-
+//const mongoose = require('mongoose');
+require('dotenv').config()
+const con = require('./MongooseConnection')
+const Rutas = require('./routes/ProductsRoutes.js')
 //const MongoClient = require('mongodb').MongoClient
 //const conn_String = 'mongodb+srv://JonathanJ:420019jJ@cluster0.oqnajti.mongodb.net/?retryWrites=true&w=majority'
 app.use(express.json());
 //const Rutas = require('./routes/web');
-//app.use('/api/v1',Rutas);
+app.use('/api/v1',Rutas);
 //const { Connection } = require('./Connection');
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+
+  /*console.log(`Example app listening on port ${port}!`)
   try {
     mongoose.connect(process.env.CONN_STRING).then(()=>{
       const Cat = mongoose.model('Cat', { name: String });
@@ -22,5 +24,5 @@ app.listen(port, () => {
     console.log('Connected to Mongo');
   } catch (error) {
     console.log('Error connecting to Mongo'+ error)
-  }
+  }*/
 })
